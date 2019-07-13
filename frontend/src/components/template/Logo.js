00bb917a84c276306ba4399a6e08763e252f4467
cleitomar <cleitomarrodrigues@gmail.com>
2019-07-13 16:41:18 -0300

    Start

---
+++ frontend/src/components/template/Logo.js
@@ -0,0 +1,12 @@
+import './Logo.css'
+import React from 'react'
+import logo from '../../assets/img/origin-logo.png'
+import { Link } from 'react-router-dom'
+
+export default props=>
+    <aside className='logo'>
+        <Link to="/" className="logo">
+            {/* <img src={logo} alt="logo"/> */}
+            <h1>SW</h1>
+        </Link>
+    </aside>
